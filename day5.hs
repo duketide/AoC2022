8119b08c9e35f86fffb402d9ccee50b460d8993c
Brady McDaniel <duketide11@gmail.com>
2022-12-05 00:46:26 -0600

    eliminate redundant funcs day 5

--- day5.hs
+++ day5.hs
@@ -17,40 +17,27 @@ stacks = M.fromList $ zip [1..] [one, two, three, four, five, six, seven, eight,
 
 type Crates = M.Map Int String
 
-moveFrom :: Crates -> [Int] -> (Crates, Int, String)
-moveFrom myMap [num, start, end] = (M.insert start newStart myMap, end, newEnd) 
+moveFrom :: Crates -> [Int] -> Bool -> (Crates, Int, String)
+moveFrom myMap [num, start, end] bool = (M.insert start newStart myMap, end, newEnd) 
   where
    starter  = resolver $ M.lookup start myMap
    newStart = drop num starter 
-   newEnd   = reverse (take num starter) ++ resolver (M.lookup end myMap)
-
---Just remove the "reverse" from newEnd
-moveFrom' :: Crates -> [Int] -> (Crates, Int, String)
-moveFrom' myMap [num, start, end] = (M.insert start newStart myMap, end, newEnd) 
-  where
-   starter  = resolver $ M.lookup start myMap
-   newStart = drop num starter 
-   newEnd   = take num starter ++ resolver (M.lookup end myMap)
+   deposit  = take num starter
+   newEnd   = (if bool then reverse deposit else deposit) ++ resolver (M.lookup end myMap)
 
 moveTo :: (Crates, Int, String) -> Crates 
 moveTo (myMap, end, newEnd) = M.insert end newEnd myMap
 
-moveIter :: [Int] -> Crates -> Crates
-moveIter move myMap = moveTo $ moveFrom myMap move
+moveIter :: [Int] -> Crates -> Bool -> Crates
+moveIter move myMap bool = moveTo $ moveFrom myMap move bool
 
-moveIter' :: [Int] -> Crates -> Crates
-moveIter' move myMap = moveTo $ moveFrom' myMap move
-
-makeMoves :: [[Int]] -> Crates -> Crates 
-makeMoves moves myMap = foldl (flip moveIter) myMap moves
-
-makeMoves' :: [[Int]] -> Crates -> Crates 
-makeMoves' moves myMap = foldl (flip moveIter') myMap moves
+makeMoves :: [[Int]] -> Crates -> Bool -> Crates 
+makeMoves moves myMap bool = foldl (\acc x -> moveIter x acc bool) myMap moves
 
 main = do
   rawInput <- readFile "day5.txt"
   let moveInput = map (map readInt . words . \str -> filter (`notElem` "movefrt") str) $ lines rawInput
-  let solution1 = map (head . snd) $ M.toList $ makeMoves moveInput stacks 
+  let solution1 = map (head . snd) $ M.toList $ makeMoves moveInput stacks True 
   print solution1
-  let solution2 = map (head . snd) $ M.toList $ makeMoves' moveInput stacks 
+  let solution2 = map (head . snd) $ M.toList $ makeMoves moveInput stacks False 
   print solution2
