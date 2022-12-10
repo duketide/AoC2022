fc2313e8136b807ce233b448d60e8985ff53c8b0
Brady McDaniel <duketide11@gmail.com>
2022-12-10 10:26:24 -0600

    day 10 clean-up

--- day10.hs
+++ day10.hs
@@ -1,7 +1,6 @@
-import MyUtils
-import Data.List
-import Data.List.Split
-import Data.Char
+import MyUtils (readInt)
+import Data.List.Split (chunksOf)
+import Data.Char (isAlpha)
 
 counter :: [String] -> Int -> Int
 counter strs int = int * (1 + sum (map readInt $ filter  (not . isAlpha . head) $ concatMap words $ filter (\x -> head x == 'a') $ take (int -1) strs))
@@ -19,11 +18,6 @@ main = do
      moves = map (\x -> if length x > 1 then readInt $ last x else 0) input2
      posByCycle = init $ 1 : sprtPos 1 moves []
      posWithCycle = zip posByCycle (map (`mod` 40) [0..])
-     result = map (\x -> if abs (uncurry (-) x) < 2 then "#" else ".") posWithCycle
+     result = unlines $ chunksOf 40 $ concatMap (\x -> if abs (uncurry (-) x) < 2 then "#" else ".") posWithCycle
  print $ total 20 + total 60 + total 100 + total 140 + total 180 + total 220 
- print $ take 40 result
- print $ take 40 (drop 40 result)
- print $ take 40 (drop 80 result)
- print $ take 40 (drop 120 result)
- print $ take 40 (drop 160 result)
- print $ take 40 (drop 200 result)
+ putStr result
