be9d9e03d17934547b86884b2fbf38ea1d742412
Brady McDaniel <duketide11@gmail.com>
2022-12-10 10:17:20 -0600

    day 10

---
+++ day10.hs
@@ -0,0 +1,29 @@
+import MyUtils
+import Data.List
+import Data.List.Split
+import Data.Char
+
+counter :: [String] -> Int -> Int
+counter strs int = int * (1 + sum (map readInt $ filter  (not . isAlpha . head) $ concatMap words $ filter (\x -> head x == 'a') $ take (int -1) strs))
+
+sprtPos :: Int -> [Int] -> [Int] -> [Int]
+sprtPos _ []    result = reverse result 
+sprtPos n moves result = sprtPos (n + head moves) (tail moves) ((n + head moves) : result)
+
+main = do
+ rawInput <- readFile "day10.txt"
+ let input = lines rawInput
+     padded = concatMap (\x -> if head x == 'a' then ["addx 0", x] else [x]) input
+     total = counter padded
+     input2 = map words padded
+     moves = map (\x -> if length x > 1 then readInt $ last x else 0) input2
+     posByCycle = init $ 1 : sprtPos 1 moves []
+     posWithCycle = zip posByCycle (map (`mod` 40) [0..])
+     result = map (\x -> if abs (uncurry (-) x) < 2 then "#" else ".") posWithCycle
+ print $ total 20 + total 60 + total 100 + total 140 + total 180 + total 220 
+ print $ take 40 result
+ print $ take 40 (drop 40 result)
+ print $ take 40 (drop 80 result)
+ print $ take 40 (drop 120 result)
+ print $ take 40 (drop 160 result)
+ print $ take 40 (drop 200 result)
