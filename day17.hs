623d3146299dee9a77b8406f02567d863f9fd970
Brady McDaniel <duketide11@gmail.com>
2022-12-18 20:24:28 -0600

    day 17 add part 2

--- day17.hs
+++ day17.hs
@@ -1,8 +1,9 @@
 import MyUtils
 import Data.Set (Set)
 import qualified Data.Set as S
-import Data.List (intersperse)
+import Data.List (group, intersperse, sortBy)
 import Data.Bifunctor (bimap)
+import Data.Function (on)
 
 type Point = (Int, Int)
 data Move = D | R | L deriving (Eq, Show)
@@ -123,12 +124,12 @@ sqr p@(x, y) rocks mv =
         | otherwise = left
   in  if bool then move p mv else p
 
-moveIter :: Int -> Set Point -> String -> Set Point
-moveIter rest starterSet moves = go starterSet Wide (2, 4) moves rest
+moveIter :: Int -> Set Point -> String -> (Set Point, [(Int, Int)])
+moveIter rest starterSet moves = go starterSet Wide (2, 4) moves rest []
   where
-    go :: Set Point -> Shape -> Point -> String -> Int -> Set Point
-    go rocks _     loc  _       0    = rocks
-    go rocks shape loc (m:mvs) restNum = go nextRocks nextShape nextLoc nextMoves nextNum
+    go :: Set Point -> Shape -> Point -> String -> Int -> [(Int, Int)] -> (Set Point, [(Int, Int)])
+    go rocks _     _   _       0       cycles = (rocks, cycles)
+    go rocks shape loc (m:mvs) restNum cycles = go filtRocks nextShape nextLoc nextMoves nextNum nextCycles
       where
         mv
           | m == '<'  = L
@@ -140,11 +141,12 @@ moveIter rest starterSet moves = go starterSet Wide (2, 4) moves rest
           | not toRest        = tryLoc
           | nextShape == Crs  = (3, highest + 4)
           | otherwise         = (2, highest + 4)
-        highest = maximum $ S.map snd nextRocks
+        highest = maximum $ S.map snd filtRocks
         nextMoves = if null mvs then moves else mvs 
         toRest = mv == D && tryLoc == loc
         nextShape = if toRest then succ shape else shape
         nextNum = if toRest then restNum - 1 else restNum
+        nextCycles = if nextMoves == moves then (rest - nextNum, highest) : cycles else cycles
         wideEx = mapper [(1, 0), (2, 0), (3, 0)]
         tallEx = mapper [(0, 1), (0, 2), (0, 3)]
         crsEx = mapper [(0, 1), (0, 2), (1, 1), (-1, 1)]
@@ -152,7 +154,9 @@ moveIter rest starterSet moves = go starterSet Wide (2, 4) moves rest
         sqrEx = mapper [(1, 0), (0, 1), (1, 1)]
         folder = foldr S.insert rocks
         mapper xs = map (bimap (+ fst loc) (+ snd loc)) $ (0, 0) : xs
-        nextRocks = if not toRest then rocks else case shape of
+        maxLn = maximum $ S.map snd $ S.filter (\(_, y) -> S.member (0, y) nextRocks && S.member (1, y) nextRocks && S.member (2, y) nextRocks && S.member (3, y) nextRocks && S.member (4, y) nextRocks && S.member (5, y) nextRocks && S.member (6, y) nextRocks ) nextRocks
+        filtRocks = if not toRest then rocks else S.filter (\(_, y) -> y >= maxLn) nextRocks
+        nextRocks = case shape of
                    Wide -> folder wideEx
                    Tall -> folder tallEx
                    Crs -> folder crsEx
@@ -163,4 +167,14 @@ main = do
   rawInput <- readFile "day17.txt"
   let startSet = S.fromList $ zip [0..6] (repeat 0)
       input = trim $ intersperse 'D' rawInput
-  print $ maximum $ S.map snd $ moveIter 2022 startSet input
+      result = moveIter 2022 startSet input
+      result' = moveIter (length input) startSet input
+      cycles = reverse $ snd result'
+      grpCycles = sortBy (compare `on` length) $ group $ snd $ foldl (\((x, y), diffs) (x2, y2) -> ((x2, y2), (x2 - x, y2 - y) : diffs)) ((0, 0), []) cycles
+      (shapesOffTheTop, heightOffTheTop) = head $ head grpCycles
+      (shapeCycle, heightCycle) = head $ last grpCycles
+      cyclesNeeded = (10^12 - shapesOffTheTop) `div` shapeCycle
+      remainderNeeded = 10^12 - (cyclesNeeded * shapeCycle) - shapesOffTheTop
+      heightAfterCycles = heightOffTheTop + (cyclesNeeded * heightCycle)
+  print $ maximum $ S.map snd $ fst result
+  print $ heightAfterCycles + (maximum (S.map snd $ fst $ moveIter (shapesOffTheTop + remainderNeeded) startSet input) - heightOffTheTop)
