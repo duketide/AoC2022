ef0d25082a6d014981a5ea5fff0abf931b8cea28
Brady McDaniel <duketide11@gmail.com>
2022-12-24 01:12:59 -0600

    day 24 part 1

--- day24.hs
+++ day24.hs
@@ -6,37 +6,9 @@ import qualified Data.Set as S
 import Data.Maybe (fromMaybe)
 
 type Point = (Int, Int)
-data Tile = Wall | B {n :: Bool, e :: Bool, s :: Bool, w :: Bool} deriving (Show, Ord, Eq)
-type Valley = Map Point (Point, Tile)
 type Blizzards = Set Blizzard
 data Dir = N | E | W | S | Remove deriving (Show, Ord, Eq)
 type Blizzard = (Point, Dir)
-
-{-inputMapper :: [[(Int, Char)]] -> Valley
-inputMapper input = go 0 input []
-  where
-    go :: Int -> [[(Int, Char)]] -> [[(Point, (Point, Tile))]] -> Valley
-    go _ []    mapped = M.fromList $ concat mapped
-    go n (u:m) mapped = go (n +1) m (map (\(x, t) -> ((x,n), ((x, n), tile t))) u : mapped)
-    open = B {n = False, e = False, s = False, w = False}
-    tile t = case t of
-      '#' -> Wall
-      '.' -> open
-      '>' -> open {e=True}
-      'v' -> open {s=True} 
-      '<' -> open {w=True} 
-      '^' -> open {n=True} 
-
-mapIter :: Valley -> Valley
-mapIter v = M.map (\t@(x, y) -> if y == Wall then t else (x, B {n = xn x, e = xe x, s = xs x, w = xw x})) v
-  where 
-    xn x = n $ v M.! nNeighbor
-    xs x = s $ v M.! sNeighbor
-    xe x = e $ v M.! eNeighbor
-    xw x = w $ v M.! wNeighbor
-    nNeighbor' (x, y) = M.lookup 
--}
-
 
 blizzardMapper :: [[(Int, Char)]] -> Blizzards 
 blizzardMapper input = go 0 input []
@@ -52,6 +24,13 @@ blizzardMapper input = go 0 input []
       '<' -> W
       '^' -> N
 
+floorMapper :: [[(Int, Char)]] -> Set Point 
+floorMapper input = go 0 input []
+  where
+    go :: Int -> [[(Int, Char)]] -> [[(Point, Char)]] -> Set Point 
+    go _ []    set = S.map fst $ S.filter (\(x, y) -> y /= '#') $ S.fromList $ concat set
+    go n (u:m) set = go (n +1) m (map (\(x, t) -> ((x, n), t)) u : set)
+
 blizzardIter :: Blizzards -> (Int, Int) -> (Int, Int) -> Blizzards
 blizzardIter b (wBound, eBound) (sBound, nBound) = S.map mapper b
   where
@@ -60,30 +39,38 @@ blizzardIter b (wBound, eBound) (sBound, nBound) = S.map mapper b
       where
         newX
          | dir == N || dir == S = x
-         | dir == E = if newX' > eBound then wBound else x + 1
-         | dir == W = if newX' < wBound then eBound else x - 1
+         | dir == E = if x+1 > eBound then wBound else x + 1
+         | dir == W = if x-1 < wBound then eBound else x - 1
         newY
          | dir == E || dir == W = y
-         | dir == N = if newX' < nBound then sBound else y - 1
-         | dir == S = if newX' > sBound then nBound else y + 1
-        newX'
-         | dir == E = x + 1
-         | dir == W = x - 1
-         | otherwise = x
-        newY'
-         | dir == S = y + 1
-         | dir == N = y - 1
-         | otherwise = y
+         | dir == N = if y-1 < nBound then sBound else y - 1
+         | dir == S = if y+1 > sBound then nBound else y + 1
+
+singleTurn :: Set Point -> Set Point -> Blizzards -> Set Point
+singleTurn locs poss blizz = S.filter (`S.member` blizPoss) $ S.foldr (\(x, y) acc -> S.insert (x, y) $ S.insert (x+1, y) $ S.insert (x-1, y) $ S.insert (x, y+1) $ S.insert (x, y-1) acc)  S.empty locs
+  where
+    blizPoss = S.filter (\x -> not $ S.member x blizz') poss
+    blizz' = S.map fst blizz
+
+solver :: (Int, Int) -> (Int, Int) -> Set Point -> Set Point -> Blizzards -> Int
+solver we sn locs poss blizz = go locs blizz 0
+  where
+    go :: Set Point -> Blizzards -> Int -> Int
+    go locs blizz n
+      | S.member (100, 36) locs = n
+      | otherwise               = go (singleTurn locs poss nextBlizz) nextBlizz (n + 1)
+          where
+            nextBlizz = blizzardIter blizz we sn
 
 main = do
   rawInput <- readFile "day24.txt"
   let input = map (zip [0..]) $ lines rawInput
       blizzards = blizzardMapper input
+      floor = floorMapper input
       wBound = 1
       nBound = 1
-      sBound = length input - 1
-      eBound = length (head input) - 1
+      sBound = length input - 2
+      eBound = length (head input) - 2
       we = (wBound, eBound)
       sn = (sBound, nBound)
-
-  print $ blizzardIter blizzards we sn
+  print $ solver we sn (S.singleton (1, 0)) floor blizzards
