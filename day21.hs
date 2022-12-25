13e225c13c5024ddcc3f6f39950db1ad7a40ccd2
Brady McDaniel <duketide11@gmail.com>
2022-12-24 20:55:01 -0600

    redo day 21 with tree

--- day21.hs
+++ day21.hs
@@ -1,90 +1,59 @@
-import MyUtils (readDouble, readInt)
-import Data.List (partition)
 import Data.Map (Map)
 import qualified Data.Map as M
-import Data.Bifunctor (second)
-import Data.Char (isAlpha)
+import Data.List (find)
+import Data.Either (isLeft, isRight, fromRight)
+import Data.Maybe (fromJust)
 
-type Nums = [(String, Double)]
-type Ops = [(String, [String])]
+type MonkeyTree = Map String (Either Int [String])
+data Monkey = M {mId :: String, payload :: Either Int [String]}
+type MonkeyTree' = Map String Monkey
 
-onceOver :: Nums -> Ops -> Ops 
-onceOver nums ops = foldl (\ops n -> map (second (map (\z -> if z == fst n then show $ snd n else z))) ops) ops nums
+readTree :: [String] -> Either Int [String]
+readTree l
+ | length l == 1 = Left (read (head l))
+ | otherwise     = Right l
 
-prune :: Ops -> (Nums, Ops)
-prune ops = result
-  where
-    (complete, incomplete) = partition (\(x, y) -> not (isAlpha (head $ head y) || isAlpha (head $ last y))) ops 
-    result = (newNums, incomplete)
-    newNums = map mapper complete
-    mapper (k, x:y:z:xs) = 
-      let nx = readDouble x; ny = readDouble y; nz = readDouble z in case y of
-      "*" -> (k, nx * nz)
-      "+" -> (k, nx + nz)
-      "-" -> (k, nx - nz)
-      "/" -> (k, nx / nz)
+opFinder :: String -> (Int -> Int -> Int)
+opFinder "+" = (+)
+opFinder "-" = (-)
+opFinder "/" = div
+opFinder "*" = (*)
 
-prune' :: Ops -> (Nums, Ops)
-prune' ops = result
-  where
-    (complete, incomplete) = partition (\(x, y) -> not (isAlpha (head $ head y) || isAlpha (head $ last y) || x == "root")) ops 
-    result = (newNums, incomplete)
-    newNums = map mapper complete
-    mapper (k, x:y:z:xs) = 
-      let nx = readDouble x; ny = readDouble y; nz = readDouble z in case y of
-      "*" -> (k, nx * nz)
-      "+" -> (k, nx + nz)
-      "-" -> (k, nx - nz)
-      "/" -> (k, nx / nz)
+eval :: String -> MonkeyTree' -> Int
+eval st mt = case payload $ mt M.! st of
+  Left int -> int
+  Right [left, op, right] -> opFinder op (eval left mt) (eval right mt)
 
-solver :: Nums -> Ops -> Nums
-solver nums ops = go nums ops []
-  where
-    go :: Nums -> Ops -> Nums -> Nums
-    go nums []  complete = nums ++ complete
-    go nums ops complete = go newNums newOps (nums ++ complete)
+finder :: String -> Monkey -> Bool
+finder node v
+  | isRight (payload v) && node `elem` fromRight [""] (payload v) = True
+  | otherwise                                                     = False
+
+revEval :: String -> MonkeyTree -> MonkeyTree' -> Int 
+revEval st mt mt'
+  | op == "==" = other
+  | op == "+" || op == "*" = upward `revOp` other
+  | otherwise = other `revOp` upward
       where
-        (newNums, newOps) = prune $ onceOver nums ops
-
-solver' :: Nums -> Ops -> (String, [String])
-solver' nums ops = go nums ops []
-  where
-    go :: Nums -> Ops -> Nums -> (String, [String])
-    go nums [x]  complete = head $ onceOver nums [x]
-    go nums ops complete = go newNums newOps (nums ++ complete)
-      where
-        (newNums, newOps) = prune' $ onceOver nums ops
-
-rootChecker :: (String, [String]) -> Double
-rootChecker (rt, x:y:z:xs) = readDouble x - readDouble z 
-
-magnitudeDeterminer :: Double -> Int
-magnitudeDeterminer db = length $ show $ abs $ round db
-
-eqFinder :: Nums -> Ops -> Int -> Double
-eqFinder nums ops mag = go (readDouble $ '9' : replicate (mag - 1) '0') (mag - 1)
-  where
-    go :: Double -> Int -> Double
-    go curr place
-     | place < 0   = 42.0 
-     | diff == 0.0 = curr
-     | diff > 0    = go (curr + 9*(10^(place - 2))) (place - 1)
-     | otherwise   = go (curr - 10^(place - 1)) place
-         where 
-           diff   = rootChecker $ solver' numMap ops
-           numMap = M.toList $ M.insert "humn" curr $ M.fromList nums
-
-
+        monkey = fromJust $ find (finder st) mt'
+        newExp = fromRight ["","",""] $ payload monkey  
+        upward = revEval (mId monkey) mt mt'
+        op = newExp !! 1
+        other' = head $ filter (\x -> length x == 4 && x /= st) newExp
+        other = eval other' mt'
+        otherIsFirst = head newExp == other'
+        revOp = case op of
+          "+" -> (-)
+          "*" -> div
+          "-" -> if otherIsFirst then (-) else (+)
+          "/" -> if otherIsFirst then div else (*)
+  
 main = do
-  rawInput <- readFile "day21.txt"
-  let input = map ((\x -> (init $ head x, tail x)) . words) $ lines rawInput
-      (nums, ops) = partition (\x -> length (snd x) == 1) input 
-      numMap = map (\(x, y) -> (x, readDouble $ head y)) nums
-      result = M.fromList $ solver numMap ops
-      humanVal = result M.! "humn"
-      numMap' = M.toList $ M.insert "humn" (humanVal + 1) $ M.fromList numMap
-      diff = rootChecker $ solver' numMap ops
-      diff2 = rootChecker $ solver' numMap' ops
-      mag = magnitudeDeterminer (diff/(diff2 - diff))
-  print $ result M.! "root"
-  print $ eqFinder numMap ops mag
+  rawInput <- readFile "AoC2022/day21.txt"
+  let input = map words $ lines rawInput
+      inputMap = foldr (\(x:xs) acc -> M.insert (init x) (readTree xs) acc) M.empty input
+      inputMap' = foldr (\(x:xs) acc -> M.insert (init x) (M {mId = init x, payload = readTree xs}) acc) M.empty input
+      [x, y, z] = fromRight ["","",""] $ inputMap M.! "root"
+      inputMap'' = M.insert "root" (M {mId = "root", payload = Right [x, "==", z]}) inputMap'
+  print $ eval "root" inputMap'
+  print $ revEval "humn" inputMap inputMap'' 
